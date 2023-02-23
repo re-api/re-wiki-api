ce1c0914e0140173db9c287ca8d911db3f65a299
Felipe Mantovani <kv4kqtl1@mailer.me>
2023-02-23 09:39:44 +0000

    task8: Implementing CharacterApi endpoint with dummy response for starters.

---
+++ re-api-rest/re-api-rest-service/rest/re/app/api/rest/service/CharactersService.java
@@ -0,0 +1,37 @@
+package rest.re.app.api.rest.service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+import rest.re.app.api.rest.api.CharactersApi;
+import rest.re.app.api.rest.api.model.GameCharacter;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class CharactersService implements CharactersApi {
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+
+    @Override
+    public Optional<ObjectMapper> getObjectMapper() {
+        return Optional.of(objectMapper);
+    }
+
+    @Override
+    public Optional<HttpServletRequest> getRequest() {
+        return Optional.empty();
+    }
+
+
+    @Override
+    public ResponseEntity<List<GameCharacter>> findCharacters() {
+        final List<GameCharacter> gameCharacters = new ArrayList<>();
+        gameCharacters.add(new GameCharacter());
+        return ResponseEntity.ok(gameCharacters);
+    }
+}
--- re-api-rest/re-api-rest-service/rest/re/app/api/rest/service/Main.java
+++ re-api-rest/re-api-rest-service/rest/re/app/api/rest/service/Main.java
@@ -1,11 +1,12 @@
 package rest.re.app.api.rest.service;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Main {
 
     public static void main(String... args){
-
+        SpringApplication.run(Main.class, args);
     }
 }
